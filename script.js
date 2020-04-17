3952ca056d6e0203c157ba26794ab7c99647c447
Kostiantyn <k.voskoboinik99@gmail.com>
2020-04-17 11:36:39 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+  //Делаем табы
+  const tab = document.querySelectorAll(".info-header-tab"),
+    info = document.querySelector(".info-header"),
+    tabContent = document.querySelectorAll(".info-tabcontent");
+  const hideTabContent = (a) => {
+    for (let i = a; i < tabContent.length; i++) {
+      tabContent[i].classList.remove("show");
+      tabContent[i].classList.add("hide");
+    }
+  };
+
+  const showTabContent = (b) => {
+    if (tabContent[b].classList.contains("hide")) {
+      tabContent[b].classList.remove("hide");
+      tabContent[b].classList.add("show");
+    }
+  };
+
+  info.addEventListener("click", (e) => {
+    let target = event.target;
+    if (target && target.classList.contains("info-header-tab")) {
+      for (let i = 0; i < tab.length; i++) {
+        if (target == tab[i]) {
+          hideTabContent(0);
+          showTabContent(i);
+          break;
+        }
+      }
+    }
+  });
+});
