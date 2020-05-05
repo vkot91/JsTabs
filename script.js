78082cfd96f499a86b4b2e88f7c67636779f29b1
Kostiantyn <k.voskoboinik99@gmail.com>
2020-05-05 12:07:35 +0300

    contact form add

--- script.js
+++ script.js
@@ -126,4 +126,79 @@ window.addEventListener("DOMContentLoaded", () => {
   // for (let i = 0; i < descrBtn.length; i++) {
   //   descrBtn[i].addEventListener("click", showPopup);
   // }
+
+  /*Form popup*/
+  let message = {
+    loading: "Loading...",
+    succes: "Thank you! We will call you soon",
+    failure: "Something go bad :(",
+  };
+  let form = document.querySelector(".main-form"),
+    input = form.querySelectorAll("input");
+  let statusMessage = document.createElement("div");
+  statusMessage.classList.add("status");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    form.appendChild(statusMessage);
+    let request = new XMLHttpRequest();
+    request.open("POST", "./server.php");
+    request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+    let formData = new FormData(form);
+    let obj = {};
+    formData.forEach(function (value, key) {
+      obj[key] = value;
+    });
+    console.log(obj);
+    let json = JSON.stringify(obj);
+
+    request.send(json);
+
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState < 4) {
+        statusMessage.innerHTML = message.loading;
+      } else if (request.readyState === 4 && request.status == 200) {
+        statusMessage.textContent = message.succes;
+      } else {
+        statusMessage.textContent = message.failure;
+      }
+    });
+    for (let i = 0; i < input.length; i++) {
+      input[i].value = "";
+    }
+  });
+  /*Contact form*/
+  let formContact = document.querySelector("#form");
+  let inputContact = formContact.querySelectorAll("input");
+  console.log(formContact, inputContact);
+  formContact.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formContact.appendChild(statusMessage);
+    let request = new XMLHttpRequest();
+    request.open("POST", "./server.php");
+    request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+    let formData = new FormData(formContact);
+    let obj = {};
+    formData.forEach(function (value, key) {
+      obj[key] = value;
+    });
+    console.log(obj);
+    let json = JSON.stringify(obj);
+    request.send(json);
+
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState < 4) {
+        statusMessage.innerHTML = message.loading;
+      } else if (request.readyState === 4 && request.status == 200) {
+        statusMessage.textContent = message.succes;
+      } else {
+        statusMessage.textContent = message.failure;
+      }
+    });
+    for (let i = 0; i < input.inputContact; i++) {
+      inputContact[i].value = "";
+    }
+  });
 });
